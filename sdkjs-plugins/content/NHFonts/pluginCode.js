6907f819fca70cf86e6d68542f7ceae3a8b057f2
nagasakimark <m.pook022@g.nagasaki-city.ed.jp>
2026-03-26 14:51:27 +0900

    Refactor NHFonts plugin code structure

--- sdkjs-plugins/content/NHFonts/pluginCode.js
+++ sdkjs-plugins/content/NHFonts/pluginCode.js
@@ -1,12 +1,9 @@
 (function () {
-
   window.Asc.plugin.init = function () {
     console.log("NHFonts plugin loaded");
   };
 
   window.Asc.plugin.button = function (id) {
-    // Close plugin when X is pressed
     this.executeCommand("close", "");
   };
-
 })();
